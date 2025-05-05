b97a89973603ebbc540f5a9ccff92e1cf67e49e6
JeisonYamarte <jeisonyamarte16@gmail.com>
2025-05-05 11:26:12 -0400

    instalacion de express, y uso de rutas para escuchar en el puerto 3000

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('My application is running!');
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res)=>{
+  res.send('MY FIRST EXPRESS APP');
+});
+
+app.get('/new_ruta', (req, res)=>{
+  res.send('Ruta Page');
+}
+);
+
+app.get('/products', (req, res)=>{
+  res.json({
+    name: 'Product 1',
+    price: 100,
+    description: 'This is a product'
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
