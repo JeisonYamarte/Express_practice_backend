97dac8a94f8c60b607bba9e0ac81a8cc28bf190a
JeisonYamarte <jeisonyamarte16@gmail.com>
2025-05-13 23:29:02 -0400

    cofiguracion de los cors lista, y listo el proyecto para produccion

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require('express');
-const {faker} = require('@faker-js/faker');
 const routerApi = require('./routes/indexRouter');
+const cors = require('cors');
+
+
 
 const {logErrors, clientErrorHandler, boomErrorHandler} = require('./middlewares/errorHandle')
 
 const app = express();
 const PORT = 3000;
 
+
 app.use(express.json());
+
+const whitelist = ['http://localhost:8080', 'https://myapp.com'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido'));
+    }
+  },
+};
+app.use(cors());
+
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'CORS funciona correctamente 🎉' });
+});
 
 app.get('/', (req, res)=>{
   res.send('MY FIRST EXPRESS APP');
