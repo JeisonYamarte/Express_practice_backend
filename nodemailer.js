837c53e83123633c2b51fbbb48a5262f3a8c8b07
JeisonYamarte <jeisonyamarte16@gmail.com>
2025-06-29 23:07:03 -0400

    testing nodemailer

---
+++ nodemailer.js
@@ -0,0 +1,28 @@
+const nodemailer = require("nodemailer");
+
+// Create a test account or replace with real credentials.
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // true for 465, false for other ports
+  auth: {
+    user: 'jeisonjoseyamartenaranjo@gmail.com',
+    pass: 'qrexsoyjcaezftsn',
+  }
+});
+
+// Wrap in an async IIFE so we can use await.
+(async () => {
+  const info = await transporter.sendMail({
+    from: process.env.EMAIL_USER, // sender address
+    to: "jeisonyamartedev@gmail.com",
+    subject: "Funciona nodemailer?", // Subject line
+    text: "Hello otro yo", // plain‑text body
+    html: "<b>Hello world?</b>", // HTML body
+  });
+
+  console.log("Message sent:", info.messageId);
+
+   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+})();
